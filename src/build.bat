a8f96521daef5dd400d6ce090905fcb3b1e402bf
kimlintu <kimlintu@hotmail.com>
2022-06-09 17:11:47 +0200

    Create build file
    Build file now links to the SDL2 libraries and includes the SDL2 headers

---
+++ src/build.bat
@@ -0,0 +1,16 @@
+@echo off
+
+set SDL_HEADERS_PATH="C:\Users\kimli\SDL2\include"
+
+set SDL_LIBRARY_PATH="C:\Users\kimli\SDL2\lib\x64"
+
+set LIBRARIES=SDL2.lib SDL2main.lib Shell32.lib
+
+set COMPILER_OPTIONS=/external:W4 /external:I%SDL_HEADERS_PATH% 
+
+set LINKER_OPTIONS=/LIBPATH:%SDL_LIBRARY_PATH% /subsystem:windows 
+
+IF NOT EXIST ..\build mkdir ..\build
+pushd ..\build
+cl %COMPILER_OPTIONS% ..\src\platform_win.cpp %LIBRARIES% /link %LINKER_OPTIONS%
+popd
