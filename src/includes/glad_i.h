ce913397754f8a2d5b5e9fa2ef3f9e979a74b98c
kimlintu <kimlintu@hotmail.com>
2022-06-15 09:29:06 +0200

    Put SDL and GLAD includes inside guards

---
+++ src/includes/glad_i.h
@@ -0,0 +1,6 @@
+#ifndef GLAD_I_H
+#define GLAD_I_H
+
+#include <glad/glad.h>
+
+#endif
---
+++ src/includes/sdl_gl.h
@@ -0,0 +1,7 @@
+#ifndef SDL_GL_INCLUDE
+#define SDL_GL_INCLUDE
+
+#include <SDL.h>
+#include <SDL_opengl.h>
+
+#endif
